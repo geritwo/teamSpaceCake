f2c718b8fbfa7e262644365c92aefa5d9f70071a
geritwo <geritwo@gmail.com>
2017-01-26 16:42:19 +0100

    JS files reorganised, satellite marker gfx added

---
+++ display.js
@@ -0,0 +1,35 @@
+var display = (function () {
+  //var issCoords = nasaAPI.getISSCoords();
+
+  var options = {
+          sky: true,
+          atmosphere: true,
+          dragging: true,
+          tilting: true,
+          zooming: false,
+          center: [46.8011, 8.2266],
+          zoom: 2
+  };
+
+  var earth = new WE.map('earth_div', options);
+  earth.setView([46.8011, 8.2266], 2);
+
+  // Texture
+  WE.tileLayer('http://data.webglearth.com/natural-earth-color/{z}/{x}/{y}.jpg', {
+    tileSize: 256,
+    bounds: [[-85, -180], [85, 180]],
+    minZoom: 0,
+    maxZoom: 16,
+    attribution: 'WebGLEarth example',
+    tms: true
+  }).addTo(earth);
+
+  // Add marker
+  var marker = WE.marker([200, 10], "./assets/satellite-station.svg", 60, 60).addTo(earth)
+  marker.bindPopup('<b>ISS marker position.</b>');
+
+  return {
+    marker: marker,
+    earth : earth
+  }
+})();
