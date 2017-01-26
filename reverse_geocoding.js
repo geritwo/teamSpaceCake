7ba8db1ca407454c8048e9fcb90bba61091ac6d3
geritwo <geritwo@gmail.com>
2017-01-26 22:37:21 +0100

    ISS position placename stored

--- reverse_geocoding.js
+++ reverse_geocoding.js
@@ -1,16 +1,20 @@
 'use strict';
 
+var placeName = [];
+
 var geocodeLatLng = function (latitude, longitude) {
    latitude = (typeof latitude !== 'undefined') ?  latitude : 47.507335;
    longitude = (typeof longitude !== 'undefined') ?  longitude : 19.065992;
    var geocoder = new google.maps.Geocoder;
    var infowindow = new google.maps.InfoWindow;
    var latlng = {lat: latitude, lng: longitude};
+
    geocoder.geocode({'location': latlng}, function(results, status) {
       if (status === 'OK') {
          if (results[1]) {
             results.forEach(function(e){
-               console.log(e.formatted_address);
+              // Here store place names - all versions
+              placeName.push(e.formatted_address);
             })
          } else {
             console.log('No results found');
@@ -19,4 +23,5 @@ var geocodeLatLng = function (latitude, longitude) {
          console.log('Geocoder failed due to: ' + status);
       }
  });
-}
+ console.log(placeName); // NOTE: indev
+};
