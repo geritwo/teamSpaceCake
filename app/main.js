9737015cd456ad4b87e991c8ea0c07e04ad824e1
tamasc <tamas.czerjak@gmail.com>
2017-01-27 03:54:11 +0100

    remove delete path

--- app/main.js
+++ app/main.js
@@ -15,15 +15,7 @@ document.querySelector('#position').addEventListener('click', function () {
   ajax.open('GET', 'http://api.open-notify.org/iss-now.json/', '', updateISSInfo);
 });
 
-<<<<<<< HEAD:client/main.js
-document.querySelector('#deletePath').addEventListener('click', function () {
-  display.deletePath();
-});
-
-
-=======
 // Update ISS info
->>>>>>> 0450528fdc9a87035df0f6cc659fa0f8dc7335fa:app/main.js
 function updateISSInfo(issPos) {
   var lat = issPos.iss_position.latitude;
   var lon = issPos.iss_position.longitude;
