cc1203beaa549bb5f73bf1f188dacc4096fabb6d
geritwo <geritwo@gmail.com>
2017-01-27 05:24:50 +0100

    Layout pimped. Clock added, not working. BUG: Update position not working? Only path.

--- app/display.js
+++ app/display.js
@@ -38,13 +38,12 @@ var display = (function () {
     var arrOfPosition = [lat, lon]; // Because built-in method needs an array.
     ISSMarker.setLatLng(arrOfPosition);
     // earth.setView(arrOfPosition);
-    ISSMarker.bindPopup('<h3>Live camera feed from the ISS:</h3><iframe width="300" height="169" src="https://www.youtube.com/embed/SF7FUU7CThs" frameborder="0" allowfullscreen></iframe>');
+    ISSMarker.bindPopup('<h3>Live camera feed from the ISS:</h3><iframe width="300" height="auto" src="http://www.ustream.tv/embed/9408562?html5ui" scrolling="no" allowfullscreen webkitallowfullscreen frameborder="0" style="border: 0 none transparent;"></iframe>');
     // ISSMarker.bindPopup('<b>ISS marker position.</b><br>latitude: '+ arrOfPosition[0] + ', longitude: ' +   arrOfPosition[1]);
     positions.push(arrOfPosition);
   };
 
   var getMarkerPosition = function() {
-
   }
 
   var showPath = function () {
@@ -57,6 +56,35 @@ var display = (function () {
     }
   };
 
+  var dateTime = function(id) {
+          date = new Date;
+          year = date.getFullYear();
+          month = date.getMonth();
+          months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+          d = date.getDate();
+          day = date.getDay();
+          days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+          h = date.getHours();
+          if(h<10)
+          {
+                  h = "0"+h;
+          }
+          m = date.getMinutes();
+          if(m<10)
+          {
+                  m = "0"+m;
+          }
+          s = date.getSeconds();
+          if(s<10)
+          {
+                  s = "0"+s;
+          }
+          result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+          document.getElementById(id).innerHTML = result;
+          setTimeout('date_time("'+id+'");','1000');
+          return true;
+  };
+
   // var deletePath = function () {
   //   delete.earth.
   // }
@@ -64,7 +92,7 @@ var display = (function () {
   var renderPlaceName = function () {
     var target = document.querySelector('.iss_is_above');
     target.innerHTML = "The ISS is currently above " + placeName + ".";
-  }
+  };
 
   return {
     earth : earth,
@@ -72,5 +100,6 @@ var display = (function () {
     setMarker: setMarkerPosition,
     showPath: showPath,
     renderPlaceName : renderPlaceName,
+    dateTime: dateTime,
   }
 })();
